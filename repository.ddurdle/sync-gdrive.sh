9f6a2140eda95d01efe448f14b4c6748ef75295f
Dylan Durdle <ddurdle@gmail.com>
2016-08-25 00:51:33 -0400

    new gdrive

--- repository.ddurdle/sync-gdrive.sh
+++ repository.ddurdle/sync-gdrive.sh
@@ -1,2 +1,2 @@
-rsync -avix --progress --exclude-from '.git' ../XBMC-gdrive/* repo/plugin.video.gdrive
-rsync -avix --progress --exclude-from '.git' ../XBMC-gdrive-0.3-dev/* repo.princeoliver/plugin.video.gdrive
+rsync -avix --progress  ../../XBMC-gdrive/* plugin.video.gdrive
+#rsync -avix --progress --exclude-from '.git' ../XBMC-gdrive-0.3-dev/* repo.princeoliver/plugin.video.gdrive

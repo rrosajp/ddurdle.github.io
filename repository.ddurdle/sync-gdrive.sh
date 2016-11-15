07ace29705bc01d1dc2c9de6335a6916117bd6c6
Dylan Durdle <ddurdle@gmail.com>
2016-11-15 07:50:46 -0500

    new gdrive

--- repository.ddurdle/sync-gdrive.sh
+++ repository.ddurdle/sync-gdrive.sh
@@ -1,3 +1,3 @@
-rsync -avix --progress  ../../XBMC-gdrive/* plugin.video.gdrive
+rsync -avix --progress  ../../GDrive-for-KODI/* plugin.video.gdrive
 #rsync -avix --progress --exclude-from '.git' ../XBMC-gdrive-0.3-dev/* repo.princeoliver/plugin.video.gdrive
 

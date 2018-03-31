8a6cdacfd8b4e3133c2fb4d5dc3898e55ccda83a
Dylan Durdle <ddurdle@gmail.com>
2018-03-30 20:35:28 -0400

    updated 0.8.64 (fix)

--- repository.ddurdle-testing/sync-gdrive.sh
+++ repository.ddurdle-testing/sync-gdrive.sh
@@ -1,1 +1,3 @@
 rsync -avix --progress  ../../GDrive-for-KODI/* plugin.video.gdrive-testing
+rsync -avix --progress ../../GDrive-for-KODI/resources/language/* plugin.video.gdrive-testing/resources/language
+rsync -avix --progress ../../GDrive-for-KODI/resources/lib/* plugin.video.gdrive-testing/resources/lib
